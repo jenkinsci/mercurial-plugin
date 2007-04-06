4a21cd92278cb436aa94e9c1ae518c71f0f1c38f
kohsuke <kohsuke@dev.java.net>
2007-04-06 14:45:55 +0000

    added skeleton for the mercurial plugin. Plan to work on this jointly with Jonathan Gibbons.

---
+++ src/main/java/hudson/plugins/mercurial/MercurialSCM.java
@@ -0,0 +1,71 @@
+package hudson.plugins.mercurial;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.model.TaskListener;
+import hudson.scm.ChangeLogParser;
+import hudson.scm.SCM;
+import hudson.scm.SCMDescriptor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Mercurial SCM.
+ * 
+ * @author Kohsuke Kawaguchi
+ */
+public class MercurialSCM extends SCM {
+    @Override
+    public boolean pollChanges(AbstractProject project, Launcher launcher, FilePath workspace, TaskListener listener) throws IOException, InterruptedException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean checkout(AbstractBuild build, Launcher launcher, FilePath workspace, BuildListener listener, File changelogFile) throws IOException, InterruptedException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void buildEnvVars(Map<String, String> env) {
+    }
+
+    @Override
+    public FilePath getModuleRoot(FilePath workspace) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ChangeLogParser createChangeLogParser() {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return DescriptorImpl.DESCRIPTOR;
+    }
+
+    public static final class DescriptorImpl extends SCMDescriptor<MercurialSCM> {
+        public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+        private  DescriptorImpl() {
+            super(MercurialSCM.class, null);
+        }
+
+        public String getDisplayName() {
+            return "Mercurial";
+        }
+
+        public SCM newInstance(StaplerRequest req) throws FormException {
+            return req.bindParameters(MercurialSCM.class,"mercurial.");
+        }
+    }
+}
