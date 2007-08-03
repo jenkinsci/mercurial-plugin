ccfc3176382163357469c03b6500985aea84b6b7
kohsuke <kohsuke@dev.java.net>
2007-08-03 04:01:31 +0000

    implemented changelog parser.

--- src/main/java/hudson/plugins/mercurial/MercurialChangeLogParser.java
+++ src/main/java/hudson/plugins/mercurial/MercurialChangeLogParser.java
@@ -2,17 +2,42 @@ package hudson.plugins.mercurial;
 
 import hudson.model.AbstractBuild;
 import hudson.scm.ChangeLogParser;
+import hudson.util.IOException2;
+import org.apache.commons.digester.Digester;
 import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
+ * Parses the changelog.xml.
+ *
+ * See {@link MercurialChangeSet#CHANGELOG_TEMPLATE} for the format.
+ * 
  * @author Kohsuke Kawaguchi
  */
 public class MercurialChangeLogParser extends ChangeLogParser {
     public MercurialChangeSetList parse(AbstractBuild build, File changelogFile) throws IOException, SAXException {
-        // TODO
-        throw new UnsupportedOperationException();
+        Digester digester = new Digester();
+        ArrayList<MercurialChangeSet> r = new ArrayList<MercurialChangeSet>();
+        digester.push(r);
+
+        digester.addObjectCreate("*/changset", MercurialChangeSet.class);
+        digester.addSetProperties("*/changset");
+        digester.addBeanPropertySetter("*/changset/msg");
+        digester.addBeanPropertySetter("*/changset/added");
+        digester.addBeanPropertySetter("*/changset/deleted");
+        digester.addBeanPropertySetter("*/changset/files");
+
+        try {
+            digester.parse(changelogFile);
+        } catch (IOException e) {
+            throw new IOException2("Failed to parse "+changelogFile,e);
+        } catch (SAXException e) {
+            throw new IOException2("Failed to parse "+changelogFile,e);
+        }
+
+        return new MercurialChangeSetList(build,r);
     }
 }
