54cd3fd3f19cd5f5ae6c082d426e4418766618bd
kohsuke <kohsuke@dev.java.net>
2009-09-15 01:10:33 +0000

    added backward compatibility measure.

--- src/main/java/hudson/plugins/mercurial/browser/HgBrowser.java
+++ src/main/java/hudson/plugins/mercurial/browser/HgBrowser.java
@@ -8,22 +8,37 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Abstract parent class, as there is more than one browser.
+ * Parent class, as there is more than one browser.
+ * {@link HgBrowser#resolveObject()} will return the old default {@link HgWeb}.
+ * Direct calls on this class will always throw {@link UnsupportedOperationException}s.
  */
-public abstract class HgBrowser extends RepositoryBrowser<MercurialChangeSet> {
+public class HgBrowser extends RepositoryBrowser<MercurialChangeSet> {
 
-    private final URL url;
-    
+    private URL url;
+
     private static final long serialVersionUID = 1L;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public abstract URL getChangeSetLink(MercurialChangeSet changeset) throws IOException;
+    public URL getChangeSetLink(MercurialChangeSet changeset) throws IOException {
+        throw new UnsupportedOperationException("Method is not implemented for HgBrowser");
+    }
 
-    public HgBrowser(String url) throws MalformedURLException {
+    protected HgBrowser(String url) throws MalformedURLException {
         this.url = normalizeToEndWithSlash(new URL(url));
     }
-    
+
     public URL getUrl() {
     	return url;
+    }
+    
+    public Object resolveObject() {
+        try {
+            return new HgWeb(url.toExternalForm());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
