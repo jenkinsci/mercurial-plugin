a0ee9cb6c8401a19ad2ff812283ef2a0cb14eca3
scraft <steve@pawprintgames.com>
2013-07-27 13:35:56 +0100

    Do not run file system checks designed for POSIX OS's on non POSIX OS's.

--- src/test/java/hudson/plugins/mercurial/HgExeTest.java
+++ src/test/java/hudson/plugins/mercurial/HgExeTest.java
@@ -28,16 +28,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HgExeTest {
-
     @Test public void pathEquals() {
         assertTrue(HgExe.pathEquals("http://nowhere.net/hg/", "http://nowhere.net/hg/"));
         assertTrue(HgExe.pathEquals("http://nowhere.net/hg", "http://nowhere.net/hg/"));
         assertTrue(HgExe.pathEquals("http://nowhere.net/hg/", "http://nowhere.net/hg"));
         assertTrue(HgExe.pathEquals("http://nowhere.net/hg", "http://nowhere.net/hg"));
         assertFalse(HgExe.pathEquals("https://nowhere.net/hg/", "http://nowhere.net/hg/"));
-        assertTrue(HgExe.pathEquals("file:/var/hg/stuff", "/var/hg/stuff"));
-        assertTrue(HgExe.pathEquals("file:///var/hg/stuff", "/var/hg/stuff"));
-        assertFalse(HgExe.pathEquals("file:/var/hg/stuff", "/var/hg/other"));
+        if (  org.apache.commons.lang.SystemUtils.IS_OS_UNIX ) {
+            assertTrue(HgExe.pathEquals("file:/var/hg/stuff", "/var/hg/stuff"));
+            assertTrue(HgExe.pathEquals("file:///var/hg/stuff", "/var/hg/stuff"));
+            assertFalse(HgExe.pathEquals("file:/var/hg/stuff", "/var/hg/other"));
+        }
     }
-
 }
