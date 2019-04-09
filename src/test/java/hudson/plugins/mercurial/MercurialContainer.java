e8a27ba33fad9eada558017487f28f11af74dd70
Jesse Glick <jglick@cloudbees.com>
2019-04-09 16:48:27 -0400

    May as well start testing against the newest released version.

--- src/test/java/hudson/plugins/mercurial/MercurialContainer.java
+++ src/test/java/hudson/plugins/mercurial/MercurialContainer.java
@@ -75,7 +75,7 @@ public class MercurialContainer extends JavaContainer {
     public enum Version {
         HG2("2.9.2"),
         HG3("3.9.2"),
-        HG4("4.4.1");
+        HG4("4.9.1");
         public final String exactVersion;
         Version(String exactVersion) {
             this.exactVersion = exactVersion;
