ebfc0b403f6af364d9e2f317670b6c9d0816c732
René Scheibe <rene.scheibe@gmail.com>
2019-06-24 22:28:10 +0200

    Remove deprecated method override of buildEnvVars
    It's already properly overridden in jenkins-core SCM.

--- src/main/java/hudson/plugins/mercurial/MercurialSCM.java
+++ src/main/java/hudson/plugins/mercurial/MercurialSCM.java
@@ -858,14 +858,8 @@ public class MercurialSCM extends SCM implements Serializable {
         }
     }
 
-    // TODO: 2.60+ Delete this override.
     @Override
-    public void buildEnvVars(AbstractBuild<?,?> build, Map<String,String> env) {
-        buildEnvironment(build, env);
-    }
-
-    // TODO: 2.60+ - add @Override.
-    public void buildEnvironment(Run<?,?> build, Map<String, String> env) {
+    public void buildEnvironment(@NonNull Run<?, ?> build, @NonNull Map<String, String> env) {
         buildEnvVarsFromActionable(build, env);
     }
 
